ce481b57a4447a25a6771fd174ffd2838d5ab3fd
Walter Doekes <walter+github@wjd.nu>
2022-07-10 23:45:50 +0200

    Expand bogoduino with digitalWrite, HIGH, LOW

--- Arduino.cpp
+++ Arduino.cpp
@@ -4,3 +4,4 @@ void delay(unsigned long ms) {}
 unsigned long millis() { static int m = 50000; return (m += 60000); }
 void pinMode(uint8_t pin, uint8_t mode) {}
 int analogRead(uint8_t pin) { return 21; }
+void digitalWrite(uint8_t pin, uint8_t val) {}
--- Arduino.h
+++ Arduino.h
@@ -15,9 +15,14 @@
 class __FlashStringHelper;
 #define F(x) reinterpret_cast<const __FlashStringHelper*>(x)
 
+/* No need for these to be static/inline/constexpr; we run bogoduino on
+ * platforms with plenty of RAM. */
 const uint8_t A0 = 0;
 const uint8_t INPUT = 0;
 const uint8_t OUTPUT = 1;
+const uint8_t INPUT_PULLUP = 2;
+const uint8_t LOW = 0;
+const uint8_t HIGH = 1;
 
 template<class T> inline T min(T a, T b) { return (a < b) ? a : b; }
 template<class T> inline T max(T a, T b) { return (a > b) ? a : b; }
@@ -26,5 +31,6 @@ void delay(unsigned long ms);
 unsigned long millis();
 void pinMode(uint8_t pin, uint8_t mode);
 int analogRead(uint8_t pin);
+void digitalWrite(uint8_t pin, uint8_t val);
 
 #endif //INCLUDED_BOGODUINO_ARDUINO_H
